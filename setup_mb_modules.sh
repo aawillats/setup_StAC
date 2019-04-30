fa5ab40e5cb39301b1b18a348251d3027d94cee8
aawillats <46765975+aawillats@users.noreply.github.com>
2019-04-30 17:30:00 -0400

    Update setup_mb_modules.sh

--- setup_mb_modules.sh
+++ setup_mb_modules.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
+#set -e #this will unfortunately fail @ cloning rtxi
+LOCAL_HOME="/home/adam/RTXI"
+HELP_PATH="module_help"
 
+cd "$LOCAL_HOME/$HELP_PATH"
+
+git config --global credential.helper cache
+git clone https://github.com/mfbolus/StanleyRTXI.git
+
+cd StanleyRTXI
+./getSubmodules.sh
+
+git clone https://github.com/mfbolus/cpp-dynCtrlEst
+cd cpp-dynCtrlEst
+
+./getArmadillo.sh
+./install_matio.sh
+./makeArmadillo.sh
+
+
+cd ..
+git config --global credential.helper cache
+./makeModules.sh
